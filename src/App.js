12bd19305ddac016acba5d860470fd47f474c38c
Aman Chandel <amaniiitu32@gmail.com>
2022-03-12 19:27:29 +0530

    ReactFoodApp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import Header from './Components/Layout/Header';
-
+import React from "react";
+import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 function App() {
   return (
     <React.Fragment>
-      <Header/>
+      <Header />
+      <main>
+        <Meals/>
+      </main>
     </React.Fragment>
   );
 }
---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <AvailableMeals />
+      <MealsSummary />
+    </React.Fragment>
+  );
+};
+
+export default Meals;
