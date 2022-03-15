ca42d12beef31d6141f1254f3a87bc26be90ca3b
Aman Chandel <amaniiitu32@gmail.com>
2022-03-15 17:03:45 +0530

    ReactFoodApp

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,27 +1,47 @@
+import { useContext } from "react";
+import CartContext from "../../Store/Cart-Context";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
-import Modal from '../UI/Modal';
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
-  const CartItems = <ul className={classes['cart-items']}>
-    {[
-    { id: "c1", name: "Sushi", Amount: 2, price: 12.99 }].map((item) => (
-      <li>{item.name}</li>
-    ))
-  }
-  </ul>;
-     
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.TotalAmount}`;
 
-//Custom elemnets dont have built in onclick the onclick here is just a name given by us.
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
+  const CartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id) }
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
+  //Custom elemnets dont have built in onclick the onclick here is just a name given by us.
   return (
-    <Modal onClick = {props.onClose}>
+    <Modal onClick={props.onClose}>
       {CartItems}
       <div className={classes.total}>
-          <span>Total Amount</span>
-          <span>35.62</span>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-          <button className={classes['button--alt']} onClick= {props.onClose}>Close</button>
-          <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -5,10 +5,29 @@ const defaultCartState = {
     items: [],
     TotalAmount: 0
 }
+
 const cartReducer = (state, action) => {
     if(action.type === 'ADD'){
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.TotalAmount + action.item.price * action.item.amount;
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+
+    
+        let updatedItems;
+        if(existingCartItem){
+
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }else{
+            updatedItems = state.items.concat(action.item);
+        }
+
+
+
         return {
             items : updatedItems,
             TotalAmount : updatedTotalAmount
